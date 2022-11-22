ce3e3be4c19ff9ecb93851ed6cad9347706748d6
Francesco C. Lanzoni <117127113+Fclanzo@users.noreply.github.com>
2022-11-21 22:27:07 -0300

    montando formulario html

---
+++ cadastrar.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <div class=>
+            <div class="col-md-6 mb-5">
+                <h2>Cadastro</h2>
+            </div>
+            <div class="row">
+                <div class="">
+                     <div class="col-6 p-4 p-md-5 formulario">
+                        <div class="w-100">
+                            <h3>Cadastro</h3>
+                        </div>
+                            <form>
+                                <div class="row">
+                                    <div class="col-6">
+                                        <div class="form-group mb-3">
+                                            <label class="label">Nome</label>
+                                            <input type="text" class="form-control">
+                                        </div>
+                                    
+                                </div>
+                                <div class="col-6">
+                                    <div class="form-group mb-3">
+                                        <label class="label">Sobrenome</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                            </div>
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="label">E-mail</label>
+                                <input type="email" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                            <label class="label">Senha</label>
+                                <input type="password" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                            <label class="label">Confirmação da Senha</label>
+                                <input type="password" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                            <label class="label">Data de nascimento</label>
+                                <input type="date" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                            <label class="label">Endereço</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                            <label class="label">Número</label>
+                                <input type="number" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                            <label class="label">Cidade</label>
+                                <input type="text" class="form-control">
+                            </div>
+                            <div class="form-group mb-3">
+                            <label class="label">Estado</label>
+                            <select id="UF" name="UF">
+    <option value="">Selecione</option>
+    <option value="AC">AC</option>
+    <option value="AL">AL</option>
+    <option value="AP">AP</option>
+    <option value="AM">AM</option>
+    <option value="BA">BA</option>
+    <option value="CE">CE</option>
+    <option value="DF">DF</option>
+    <option value="ES">ES</option>
+    <option value="GO">GO</option>
+    <option value="MA">MA</option>
+    <option value="MS">MS</option>
+    <option value="MT">MT</option>
+    <option value="MG">MG</option>
+    <option value="PA">PA</option>
+    <option value="PB">PB</option>
+    <option value="PR">PR</option>
+    <option value="PE">PE</option>
+    <option value="PI">PI</option>
+    <option value="RJ">RJ</option>
+    <option value="RN">RN</option>
+    <option value="RS">RS</option>
+    <option value="RO">RO</option>
+    <option value="RR">RR</option>
+    <option value="SC">SC</option>
+    <option value="SP">SP</option>
+    <option value="SE">SE</option>
+    <option value="TO">TO</option>
+ </select>
+                            </div>
+                            <div class="col-6">
+                        <img class="img" src="foto1.jpg" width="300px">
+                    </div>
+            </div>            
+</head>
+<body>
+    
+</body>
+</html>
